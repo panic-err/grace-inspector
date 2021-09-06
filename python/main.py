89a9b6505defab089dac2ce00508bf7994f94026
Ben Belland <bobmakehug@gmail.com>
2021-09-06 19:23:17 -0400

    starting python work

--- python/main.py
+++ python/main.py
@@ -4,8 +4,10 @@ print(PySide6.__version__)
 
 import random
 import sys
+import os
 
-from PySide6.QtCore import Qt, Slot
+from PySide6.QtQml import QQmlApplicationEngine
+from PySide6.QtCore import QUrl, Qt, Slot
 from PySide6.QtWidgets import (QApplication, QLabel, QPushButton, QVBoxLayout, QWidget)
 from __feature__ import snake_case, true_property
 
@@ -18,24 +20,29 @@ class Hallo(QWidget):
                 "hi",
                 "hola"
                 ]
-        self.button = QPushButton("Click me!")
-        self.message = QLabel("Hallo world")
-        self.message.alignment = Qt.AlignCenter
+
+        self.buttons = []
 
         self.layout = QVBoxLayout(self)
-        self.layout.add_widget(self.message)
-        self.layout.add_widget(self.button)
-
-        self.button.clicked.connect(self.greet)
-
+        for i in range(28):
+            button = QPushButton(str(i))
+            self.layout.add_widget(button)
+            button.clicked.connect(self.greet)
+            self.buttons.append(button)
     @Slot()
     def greet(self):
-        self.message.text = random.choice(self.hello)
+        print(self)
+        #self.message.text = random.choice(self.hello)
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-
+    app = QApplication([])
+    #QtWebEngine.initialize()
+    engine = QQmlApplicationEngine()
+    qml_file_path = "../qml/button.qml"
+    qml_url = qml_file_path
+    QUrl.from_local_file(os.path.abspath(qml_file_path))
+    motor = engine.load(qml_url)
     widget = Hallo()
     widget.show()
-
-    sys.exit(app.exec_())
+    
+    sys.exit(app.exec())
