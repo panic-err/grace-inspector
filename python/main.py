8fecafd816a5fcad74e35b2d49c9c052dc34fa60
Ben Belland <bobmakehug@gmail.com>
2021-09-07 21:19:53 -0400

    grid layout and positioning

--- python/main.py
+++ python/main.py
@@ -7,7 +7,7 @@ import sys
 import os
 from PySide6.QtQml import QQmlApplicationEngine, QQmlComponent
 from PySide6.QtCore import QUrl, Qt, Slot, Property
-from PySide6.QtWidgets import (QApplication, QLabel, QPushButton, QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QLabel, QPushButton, QGridLayout, QWidget)
 from __feature__ import snake_case, true_property
 
 class Hallo(QWidget):
@@ -21,15 +21,19 @@ class Hallo(QWidget):
 
         self.buttons = []
 
-        self.layout = QVBoxLayout(self)
+        self.resize(1000, 550)
+        
+	
+        self.layout = QGridLayout(self)
         for i in range(28):
+            mess = QPushButton("Messages!")
+            self.layout.add_widget(mess, i, 0)
             button = QPushButton(str(i))
             button.position = i
-            self.layout.add_widget(button)
+            self.layout.add_widget(button, i, 1)
             button.clicked.connect(self.greet)
             #button.object_name = "butt"+str(i)
             self.buttons.append(button)
-
     @Property(QWidget)
     def buttonList():
         return self.buttons
@@ -37,7 +41,6 @@ class Hallo(QWidget):
     def greet(self):
         butt = self.focus_widget()
         print(butt.position)
-
     @Slot()
     def boop():
         print("boop")
@@ -47,15 +50,15 @@ class Hallo(QWidget):
 if __name__ == "__main__":
     app = QApplication([])
     #QtWebEngine.initialize()
+    """
     engine = QQmlApplicationEngine()
-
     qml_file_path = "../qml/window.qml"
     motor = engine.load(qml_file_path)
     button_file_path = "../qml/button.qml"
-    buttons = engine.load(button_file_path)
+    buttons = engine.load(button_file_path)"""
     widget = Hallo()
     print(widget.buttons)
-    print(engine)
+    #print(engine)
     #widget.layout.setStyleSheet("width: 450;height:550;")
     widget.show()
     sys.exit(app.exec())
