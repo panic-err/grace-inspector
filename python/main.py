9ae060b9d73809e5895a6ce68ee0e64d0c95e37c
Ben Belland <bobmakehug@gmail.com>
2021-09-13 21:55:40 -0400

    compartmentalizing pika connection

--- python/main.py
+++ python/main.py
@@ -15,31 +15,27 @@ from PySide6.QtWidgets import (QWidget, QDialog, QVBoxLayout, QApplication, QLin
 from __feature__ import snake_case, true_property
 
 class Hallo(QWidget):
-    def emission(self):
-        creds = pika.PlainCredentials(sys.argv[1], sys.argv[2])
-       
-        connection = pika.BlockingConnection(pika.ConnectionParameters(sys.argv[3], 5672, '/', creds))
-        channel = connection.channel()
-        channel.exchange_declare( exchange='topic_logs')
-        message = self.greeters[self.position].text
-        print("[x] Sent %r:%r"%(self.routing_key, message))
-        #message = 'trout'
-        channel.basic_publish(exchange='topic_logs', routing_key=self.routing_key, body=message)
-        print("[x] Sent %r:%r" %(self.routing_key, self.message) )
-        connection.close()
+    def emission(self, pos):
+        message = self.greeters[pos].text
+        print("[x] Sent %r:%r"%("trout", message))
+        
+        self.channel.basic_publish(exchange='topic_logs', routing_key="trout", body=message)
+        print("[x] Sent %r:%r" %("trout", message) )
+        
 
     def __init__(self):
+
         creds = pika.PlainCredentials(sys.argv[1], sys.argv[2])
-        connection = pika.BlockingConnection(pika.ConnectionParameters(sys.argv[3], 5672, '/', creds))
-        channel = connection.channel()
-        channel.exchange_declare(exchange='topic_logs')
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(sys.argv[3], 5672, '/', creds))
+        self.channel = self.connection.channel()
+        self.channel.exchange_declare(exchange='topic_logs')
 
         self.routing_key = 'trout'
         self.message = 'init'
-        channel.basic_publish(exchange='topic_logs', routing_key=self.routing_key, body=self.message)
+        self.channel.basic_publish(exchange='topic_logs', routing_key=self.routing_key, body=self.message)
         print("[x] Sent %r:%r" % (self.routing_key, self.message))
-        connection.close()
-
+        
+        
         QWidget.__init__(self)
         self.hello =  [
                 "hallo",
@@ -59,6 +55,7 @@ class Hallo(QWidget):
             mess.setStyleSheet("color:aqua;")
             self.layout.add_widget(mess, i, 0)
             nameButton = QPushButton("NAME")
+            nameButton.clicked.connect(self.name_detail)
             nameButton.setStyleSheet("color:aqua;")
             self.layout.add_widget(nameButton, i, 1)
             button = QPushButton(str(i))
@@ -73,18 +70,22 @@ class Hallo(QWidget):
             self.nameDetailLayout = QVBoxLayout(self.nameDetail)
             nameDetail = "Details"
             labelDetail = QPushButton(nameDetail)
-            labelDetail.clicked.connect(self.emission)
+            #labelDetail.clicked.connect(self.emission)
             self.nameDetailLayout.add_widget(labelDetail)
     @Property(QWidget)
     def buttonList():
         return self.buttons
+
     @Slot()
-    def greet(self):
-        butt = self.focus_widget()
+    def name_detail(self):
         self.nameDetail.resize(450, 500)
         self.nameDetail.setStyleSheet("color:pink;")
         self.nameDetail.show()
-        self.emission()
+    @Slot()
+    def greet(self):
+        butt = self.focus_widget()
+        self.emission(butt.position)
+        print("Butt  number"+str(butt.position)) 
         print(self.greeters[butt.position].text)
         print(butt.position)
     @Slot()
@@ -109,3 +110,5 @@ if __name__ == "__main__":
     #widget.layout.setStyleSheet("width: 450;height:550;")
     widget.show()
     sys.exit(app.exec())
+    widget.connection.close()
+    
